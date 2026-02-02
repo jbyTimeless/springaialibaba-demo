ddb48383d6428f351e6694a2e0c2bec9d6485550
贾伯言 <1741245720@qq.com>
2026-02-02 16:00:35 +0800

    Saa08Embed2vector

---
+++ SAA-08Embed2vector/src/main/java/com/boyan/saa/Saa08Embed2vectorApplication.java
@@ -0,0 +1,11 @@
+package com.boyan.saa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Saa08Embed2vectorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Saa08Embed2vectorApplication.class, args);
+    }
+}
